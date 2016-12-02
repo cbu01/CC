b9dfccc5f8224b8c24e96ab8bc99db9da475040f
mannsi <mannsi1983@gmail.com>
2016-12-02 14:15:17 +0000

    Cleaning up input code for find_x function

--- 12-01/runnerPowBenchmark.py
+++ 12-01/runnerPowBenchmark.py
@@ -2,31 +2,13 @@
 
 import PowHelper
 from timeit import default_timer as timer
-import binascii
 
 start = timer()
 
-# s = PowHelper.generate_random_bit_string(128)
-# s = bin(int(binascii.hexlify("Super long string that will generate many bits :)"), 16))[2:130]
-s = bin(int(binascii.hexlify("to_be_or_not_be_"), 16))[2:130]
-# s = "1234567891234567"
-n = 21
+s = "to_be_or_not_be_"
+n = 15
 
-x, x_int = PowHelper.find_x(s,n)
+x, x_int = PowHelper.find_x_competition(s,n)
 end = timer()
 run_time = end-start
 print "Time of run: {0} sec. Average time of hash is '{1}' sec. X found was '{2}'".format(run_time, run_time/x_int , x_int)
-
-# start = timer()
-# s_in_ascii = PowHelper.binary_to_ascii(s)
-# x_verified = PowHelper.verify_hash(s_in_ascii,x,n)
-# x_verified = PowHelper.verify_hash(s,x,n)
-# end = timer()
-# run_time = end-start
-
-
-# print "Single hash run took '{0}'".format(run_time)
-# if x_verified:
-#     print "Hash is verified"
-# else:
-#     print "Hash NOT verified"
