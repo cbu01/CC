ea7b035d382a236fb190f5155f8dfe85c3837125
mannsi <mannsi1983@gmail.com>
2016-12-02 16:26:46 +0000

    Add command line arguments to the proof of work benchmark runner

--- 12-01/runnerPowBenchmark.py
+++ 12-01/runnerPowBenchmark.py
@@ -1,14 +1,33 @@
 """ Benchmarks the speed of the hashing algorithm """
 
+import sys
+
 import PowHelper
 from timeit import default_timer as timer
 
-start = timer()
+def run(s, n):
 
-s = "to_be_or_not_be_"
-n = 15
+    start = timer()
 
-x, x_int = PowHelper.find_x_competition(s,n)
-end = timer()
-run_time = end-start
-print "Time of run: {0} sec. Average time of hash is '{1}' sec. X found was '{2}'".format(run_time, run_time/x_int , x_int)
+    x, x_int = PowHelper.find_x_competition(s,n)
+    end = timer()
+    run_time = end-start
+    print "Time of run: {0} sec. Average time of hash is '{1}' sec. The int value of X found was '{2}'".format(run_time, run_time/x_int , x_int)
+
+
+if __name__ == "__main__":
+
+    if len(sys.argv) < 2:
+        print "Usage: python runnerPowBenchmark.py 16CharLongString levelAsInValue"
+        sys.exit()
+
+    s = sys.argv[1]
+    if len(s) != 16:
+        print "ERROR: The input string s has to have length equal to 16"
+        sys.exit()
+    try:
+        n = int(sys.argv[2])
+    except:
+        print "ERROR: Unable to cast level to int ..."
+        sys.exit()
+    run(s,n)
