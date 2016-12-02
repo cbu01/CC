78ecfbb99683681de608bf4ae3041bcf49e23117
mannsi <mannsi1983@gmail.com>
2016-12-02 13:48:20 +0000

    Updating benchmark method to calculate hashes for competition

--- 12-01/runnerPowBenchmark.py
+++ 12-01/runnerPowBenchmark.py
@@ -7,23 +7,26 @@ import binascii
 start = timer()
 
 # s = PowHelper.generate_random_bit_string(128)
-s = bin(int(binascii.hexlify("Super long string that will generate many bits :)"), 16))[2:130]
-n = 20
+# s = bin(int(binascii.hexlify("Super long string that will generate many bits :)"), 16))[2:130]
+s = bin(int(binascii.hexlify("to_be_or_not_be_"), 16))[2:130]
+# s = "1234567891234567"
+n = 21
 
 x, x_int = PowHelper.find_x(s,n)
 end = timer()
 run_time = end-start
 print "Time of run: {0} sec. Average time of hash is '{1}' sec. X found was '{2}'".format(run_time, run_time/x_int , x_int)
 
-start = timer()
-s_in_ascii = PowHelper.binary_to_ascii(s)
-x_verified = PowHelper.verify_hash(s_in_ascii,x,n)
-end = timer()
-run_time = end-start
+# start = timer()
+# s_in_ascii = PowHelper.binary_to_ascii(s)
+# x_verified = PowHelper.verify_hash(s_in_ascii,x,n)
+# x_verified = PowHelper.verify_hash(s,x,n)
+# end = timer()
+# run_time = end-start
 
 
-print "Single hash run took '{0}'".format(run_time)
-if x_verified:
-    print "Hash is verified"
-else:
-    print "Hash NOT verified"
+# print "Single hash run took '{0}'".format(run_time)
+# if x_verified:
+#     print "Hash is verified"
+# else:
+#     print "Hash NOT verified"
