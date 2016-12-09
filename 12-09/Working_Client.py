7abdc288a8c20eb752dbed3a9a2ab4f2728c6b23
mannsi <mannsi1983@gmail.com>
2016-12-09 17:11:06 +0000

    Indentation issues

--- 12-09/Working_Client.py
+++ 12-09/Working_Client.py
@@ -38,42 +38,42 @@ class listeningThread (threading.Thread):
                 # TODO somehow update the blocks of all calculations threads
         
 
-    class calculationThread(threading.Thread):
-        def __init__(self, threadID, name, sock, block_without_nonce, client_name):
-            threading.Thread.__init__(self)
-            self.client_name = client_name
-            self.threadID = threadID
-            self.name = name
-            self.block = block_without_nonce
-            self.next_block_found_by_someone = False
-            
-            def run(self):
-                while True:
-                    for i in range(1000):
-                        found_nonce = ProofOfWork.try_to_set_correct_nonce(self.block)
-                        if found_nonce:
-                            # Check if somebody else already found the next block
-                            if not self.next_block_found_by_someone:
-                                print "Thread {0} from client {1} found a nonce and thinks its the first one !".format(self.threadID,
-                                                                                                         self.client_name, )
-                                # Broadcast the block
-                                self.client_dict_lock.aquire
-                                serialized_block = pickle.dumps(self.block, 'wb')
-                                for c in self.client_dict:
-                                    sock.sendto(serialized_block, self.client_dict[c][0])
-                                self.client_dict_lock.release
-                                self.update_block()
-                                continue
-                            else:
-                                # Found a block but someone was faster :(
-                                print "Thread {0} from client {1} found a nonce but was too slow".format(self.threadID, self.client_name)
-                                self.update_block()
-                                continue
-                    self.update_block()
+class calculationThread(threading.Thread):
+    def __init__(self, threadID, name, sock, block_without_nonce, client_name):
+        threading.Thread.__init__(self)
+        self.client_name = client_name
+        self.threadID = threadID
+        self.name = name
+        self.block = block_without_nonce
+        self.next_block_found_by_someone = False
 
-        def update_block(self):
-            # TODO
-            pass
+        def run(self):
+            while True:
+                for i in range(1000):
+                    found_nonce = ProofOfWork.try_to_set_correct_nonce(self.block)
+                    if found_nonce:
+                        # Check if somebody else already found the next block
+                        if not self.next_block_found_by_someone:
+                            print "Thread {0} from client {1} found a nonce and thinks its the first one !".format(self.threadID,
+                                                                                                     self.client_name, )
+                            # Broadcast the block
+                            self.client_dict_lock.aquire
+                            serialized_block = pickle.dumps(self.block, 'wb')
+                            for c in self.client_dict:
+                                sock.sendto(serialized_block, self.client_dict[c][0])
+                            self.client_dict_lock.release
+                            self.update_block()
+                            continue
+                        else:
+                            # Found a block but someone was faster :(
+                            print "Thread {0} from client {1} found a nonce but was too slow".format(self.threadID, self.client_name)
+                            self.update_block()
+                            continue
+                self.update_block()
+
+    def update_block(self):
+        # TODO
+        pass
 
 def startClient(IP, PORT, client_name):
 
@@ -93,8 +93,6 @@ def startClient(IP, PORT, client_name):
     sock.sendto(ID, (central_register_ip, central_register_port), pub_key.exportKey) 
       
     client_dict_lock = threading.Lock()
-      
-    # initialize genesis here!!!!
 
     block_chain = BlockChain()
     block_without_nonce1 = create_next_block(block_chain, client_name)
@@ -108,6 +106,7 @@ def startClient(IP, PORT, client_name):
     calc_1.start()
     calc_2.start()
     calc_3.start()
+
 
 def create_next_block(block_chain, client_name):
     prev_block = block_chain.get_latest_block()
