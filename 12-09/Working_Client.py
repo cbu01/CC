95de40cb5a2fa0935c51705512eda0b4af36d145
mannsi <mannsi1983@gmail.com>
2016-12-09 15:35:32 +0000

    Sending and receiving blocks between clients

--- 12-09/Working_Client.py
+++ 12-09/Working_Client.py
@@ -4,12 +4,16 @@ import ProofOfWork
 from BlockChain import BlockChain
 from Block import Block
 import time
+import pickle
+import RSA
 
 class Working_Client:
 
     class listeningThread (threading.Thread):
-        def __init__(self, threadID, name, sock):
+        def __init__(self, threadID, name, sock, block_chain):
             threading.Thread.__init__(self)
+            self.block_chain = block_chain
+            self.sock = sock
             self.threadID = threadID
             self.name = name
       
@@ -17,22 +21,20 @@ class Working_Client:
             while True:
         
                 # listen for new messages
-                data, addr = sock.recvfrom(2048)
-                
+                data, addr = self.sock.recvfrom(2048)
+
                 # check if it is a new client
                 if (addr == (self.register_ip, self.register_port)):
                     self.client_dict_lock.aquire
                     self.client_dict[data[0]] = (data[1], RSA.importKey(data[2]))
                     self.client_dict_lock.release
                 else:
-                    # check if received suffix is correct
-        
-                    # if suffix is correct:
-        
-                        # add block to blockchain
-        
-                    # update prefix
-            
+                    deserialized_block = pickle.loads(data[0], 'rb')
+                    new_block_verified = ProofOfWork.verify_next_block_in_chain(deserialized_block, self.block_chain)
+                    if new_block_verified:
+                        self.block_chain.add_block(deserialized_block)
+
+                    # TODO somehow update the blocks of all calculations threads
         
         
     class calculationThread(threading.Thread):
@@ -52,9 +54,9 @@ class Working_Client:
                             if not self.next_block_found_by_someone:
                                 # Broadcast the block
                                 self.client_dict_lock.aquire
+                                serialized_block = pickle.dumps(self.block, 'wb')
                                 for c in self.client_dict:
-                                    # TODO should pickle the block before sending it. Unpickle it on the other side
-                                    sock.sendto(self.block, self.client_dict[c][0])
+                                    sock.sendto(serialized_block, self.client_dict[c][0])
                                 self.client_dict_lock.release
                                 self.update_block()
                                 continue
@@ -97,12 +99,14 @@ class Working_Client:
         self.client_dict_lock = threading.Lock()
 
         block_chain = BlockChain()
-        block_without_nonce = self.create_first_block_without_nonce(block_chain, client_name)
+        block_without_nonce1 = self.create_next_block(block_chain, client_name)
+        block_without_nonce2 = self.create_next_block(block_chain, client_name)
+        block_without_nonce3 = self.create_next_block(block_chain, client_name)
 
-        self.listen = self.listeningThread(1, "Listening_Thread", self.sock, block_without_nonce)
-        self.calc_1 = self.calculationThread(2, "Calculation_Thread", self.sock, block_without_nonce)
-        self.calc_2 = self.calculationThread(3, "Calculation_Thread", self.sock, block_without_nonce)
-        self.calc_3 = self.calculationThread(4, "Calculation_Thread", self.sock, block_without_nonce)
+        self.listen = self.listeningThread(1, "Listening_Thread", self.sock, block_chain)
+        self.calc_1 = self.calculationThread(2, "Calculation_Thread", self.sock, block_without_nonce1)
+        self.calc_2 = self.calculationThread(3, "Calculation_Thread", self.sock, block_without_nonce2)
+        self.calc_3 = self.calculationThread(4, "Calculation_Thread", self.sock, block_without_nonce3)
 
         self.listen.start()
         self.calc_1.start()
@@ -110,7 +114,7 @@ class Working_Client:
         self.calc_3.start()
 
 
-    def create_first_block_without_nonce(self, block_chain, client_name):
+    def create_next_block(self, block_chain, client_name):
         prev_block = block_chain.get_latest_block()
         timestamp = int(time.time())
         data = client_name
