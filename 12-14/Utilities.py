634bfbb9e6e9b01ce7cfa66c4608d0b9525c7b52
cbu01 <caroline.butschek@gmx.de>
2016-12-16 17:58:24 +0000

    keySharing ready to turn in

--- 12-14/Utilities.py
+++
@@ -1,4 +0,0 @@
-def inverse(number, mod):
-    # TODO
-    """ Returns the inverse of a number in modulus mod """
-    pass
--- 12-14/keySharing.py
+++ 12-14/keySharing.py
@@ -1,79 +1,99 @@
 from Crypto.Random import random
 import gmpy2
-import binascii
+import binascii 
 
-p = 251 # prime
-
-def get_shares(secret, n, k):
+""" Divides a secret into n blocks. k blocks should be enough to restore 
+the secret 
+@secret: the secret, that should be shared
+@n: number of blocks into which the secret should be divided
+@k: number of blocks required to determine the secret
+@p: prime 
+@return: tuple(n, k, p, list of resulting blocks) """
+def get_shares(secret, n, k, p=251):
+    
+    # check size of p
+    # depending on which ascii symbols should remain usable, this should not be 
+    # too small either...
+    # check, that p is not larger than 251, because this would cause trouble
+    if (p > 251):
+        print "Please choose a prime which is smaller or equal to 251."
     
     # generate k empty strings (the will be filled with the secrets later)
     result_list = []
     for i in range(n):
         result_list.append("")
-        
-    # generate random coefficients
-    coeff = []
-    for a in range(k-1): 
-        trying = True
-        while trying:
-            r = random.getrandbits(8) # returns long
-            r_int = int(r)
-            if r_int < p:
-                trying = False
-                coeff.append(r_int) 
-    print "coeffs " + str(coeff)
     
-    # turn to bin and add leading zeros to fill up whole bytes
+    # turn message into binary format and add leading zeros to fill up whole bytes
     binary_secret = __turnToBin(str(secret))
-    print "binary_secret " + str(binary_secret)
     
-    # iterate over the message
+    # iterate over the message byte by byte and generate 6 secrets for each byte
     while len(binary_secret) > 0:
+             
+        # generate k-1 random coefficients
+        coeff = []
+        for a in range(k-1):
+            # choose a random coefficient
+            r = random.getrandbits(8) # returns an 8 byte value (long)
+            r_int = int(r) # convert to int
+            r_int = r_int % p # ensure that int_r is not larger than p
+            coeff.append(r_int) 
         
-        # take the first byte
+        # take the first byte and convert it to integer value
+        # remove the first byte from the string
         current_byte  = binary_secret[0:8]
         binary_secret = binary_secret[8:]
-        print "current byte " + str(__turnBinToInt(current_byte))
-        print "binary secret " + str(binary_secret)
-        # convert to int
         current_int = __turnBinToInt(current_byte)     
                        
         # calculate q(i) for i in n
         for i in range(1,n+1):
             sum = current_int
+            # calculate elements for addition
             for j in range(k-1):
-                sum += i * coeff[j]**(j+1)
+                # a*(x^k)
+                sum += coeff[j]*(i**(j+1))
+            # convert to hex representation
             hex_sum = hex(int(sum % p))[2:]
-            print "result_list?? " + str(hex_sum)
+            # ensure that hex value has the right length 
+            # Eg. hex(1)[2:] returns '1' and not '01' as desired
+            if len(hex_sum) < 2:
+                hex_sum = '0'+hex_sum
+     
+            # collect all results in a list
             result_list[(i-1) % n] = result_list[(i-1) % n] + str(hex_sum)
+    
+    # merge the results of each byte for each secretholder into a string
     result = []
-    print "result_list " + str(result_list)
     for i in range(n):
         result.append((i+1,result_list[i]))        
-    return result
+    return (n,k,p,result)
 
-def combine(k,n,p,subsecrets):
+
+""" Combines the blocks of the shared secret to determine the original secret
+@n: number of blocks, into which the original secret was split into
+@k: number of blocks to determine the original secret
+@p: prime
+@sharedsecrets: list of blocks, into which the secret was split
+@return: cleartext """
+def combine(sharedsecrets, n, k, p=251):
     
-    randsubsecrets = []
-    # randomly choose k subsecrets
+    # randomly choose k sharedsecrets
+    randsecrets = []
     for i in range(k):
-        s = random.choice(subsecrets)
-        randsubsecrets.append(s)
-        subsecrets.remove(s)
+        s = random.choice(sharedsecrets)
+        randsecrets.append(s)
+        sharedsecrets.remove(s)
         
-    # convert hex secret to integer list
+    # convert hex secret of each randomly chosen sharedsecret into an integer list
     int_secrets = []
-    for r in randsubsecrets:
-        print "current secret " + str(r)
+    for r in randsecrets: # for each k
         c = []
         hex_list = r[1]
-        while len(hex_list) > 0:
+        while len(hex_list) > 0: # as long as there are still hex values to convert
             h = hex_list[:2] # get first two numbers from hex_list
             hex_list = hex_list[2:] # remove first two numbers from hex_list 
             int_value = int(h,16) # convert to integer
-            c.append(int_value)
+            c.append(int_value) 
         int_secrets.append((r[0],c)) # collect index and list of integers
-    print "int secrets: " + str(int_secrets)
     
     # get secret length as stopper for the loop
     length = len(int_secrets[0][1])   # get number of integers
@@ -89,21 +109,19 @@ def combine(k,n,p,subsecrets):
             divisor = 1
             for l in range(k):
                 if j != l:
-                    dividend = dividend * int_secrets[l][0]
-                    divisor = divisor * (int_secrets[l][0]-int_secrets[j][0]) 
+                    dividend = dividend * int_secrets[l][0] % p
+                    divisor = divisor * ((int_secrets[l][0]-int_secrets[j][0]) % p) % p
             dividend = (int_secrets[j][1][z] * dividend) % p
             divisor = gmpy2.invert((divisor % p), p)   
             sum = (sum + ((dividend * divisor) % p)) % p
-            print "sum " + str(sum)
         # save encrypted char to list
-        print "sum in z loop " + str(sum)
         sum_collector = sum_collector + chr(sum % p)
     return sum_collector
 
             
-# convert a string into its binary representation
-# @string: ascii string to convert
-# @return: binary representation of the string 
+""" convert a string into its binary representation
+    @string: ascii string to convert
+    @return: binary representation of the string """
 def __turnToBin(string):
     b = bin(int(binascii.hexlify(string), 16))[2:]
     # add leading zeros
@@ -112,24 +130,27 @@ def __turnToBin(string):
         b = '0' + b
         x -= 1
     return b 
- 
+
+""" turn binary string to int
+@b: binary string
+@return: int """ 
 def __turnBinToInt(b):
     return int(b,2)
 
-def __turnIntToHex(b):
-    return hex(int) 
  
-# turn a string from binary representation into a string of ascii characters
-# @b string in binary representation
-# @return: ascii string
-def __turnBinToString(b):
-    s = binascii.unhexlify('%x' % int(b,2))
-    return s 
- 
+""" Test """ 
 if __name__ == "__main__":
-    secret = "blah"
+    
+    # set parameters
+    n = 6
+    k = 3
+    secret = "This is a big secret. Don't tell anyone!!!"
     print "Secret to hide: " + str(secret)
-    subsecrets = get_shares(secret, 6, 3)   
-    print "The secret is divided into the following shares: "+ str(subsecrets) 
-    joinedsecret = combine(3,6,p,subsecrets)
+    
+    # encrypt
+    sharedsecrets = get_shares(secret, n, k)[3]   
+    print "The secret is divided into the following shares: "+ str(sharedsecrets) 
+
+    # recover
+    joinedsecret = combine(sharedsecrets, n, k)
     print "Restored secret: " + str(joinedsecret)
