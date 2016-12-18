fb276f7f16cba79ba407fa821be1f1bf9a009865
cbu01 <caroline.butschek@gmx.de>
2016-12-18 18:19:39 +0000

    comments on coinflip

--- 12-01/CoinFlip/Alice.py
+++ 12-01/CoinFlip/Alice.py
@@ -2,6 +2,13 @@ import socket, pickle
 from Crypto.Random import random
 from Crypto.Hash import SHA256
 
+""" Alice connects to Bob (he has to be started BEFORE Alice). She flips a coin
+and generates the hash value of the coin and a random suffix. She sends this 
+has value to Bob. Bob flips his coin and sends his hash value then to Alice.
+Alice sends the value of the coin and the suffix to Bob, so he can check, if she
+told him the truth. Bob then sends Alice his suffix and coin value and she 
+checks, if he told the truth. 
+This implementation uses the reflection server!!! """
 def AliceCoinFlip():
 	
 	host_A = "localhost"
@@ -14,13 +21,11 @@ def AliceCoinFlip():
 	buffer_size = 1024 #bytes
 
 	# initialize socket
-	
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
 	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #dont block when program crashes
 	s.bind((host_A, port_A))
 
 	# generate coinflip and cipher
-
 	b_A = random.getrandbits(1)
 	c, suffix = commit(b_A)
 	print "Coin of Alice: " + str(b_A)
@@ -31,24 +36,20 @@ def AliceCoinFlip():
 	s.sendto(data, (rServer_ip, rServer_port))
 
 	# receive cipher from Bob
-
 	recdata, addr = s.recvfrom(1024)
 	coin_b = pickle.loads(recdata)
 	c_B = coin_b[2]
 
 	# send coinflip and nonce to Bob 
-
 	data = pickle.dumps((port_A, (host_B, port_B), (b_A, suffix)))
 	s.sendto(data, (rServer_ip, rServer_port))
 
 	# receive coinflip and nonce from Bob
-
 	recdata, addr = s.recvfrom(1024)
 	coin_b = pickle.loads(recdata)
 	b_B, suffix_B = coin_b[2]
 
 	# reveal
-
 	if (reveal(b_B, c_B, suffix_B)):
 		print "Bob tells the truth"
 		result = (b_B^b_A)
@@ -57,7 +58,10 @@ def AliceCoinFlip():
 		print "Bob does not tell the truth"
 
 
-	
+""" add a random suffix to the value of the coin and generate its hash
+value
+@b_A: a random bit - can be 0 or 1 
+@return: hash value and random suffix"""	
 def commit(b_A):
 	
 	Min = 255 # bits
@@ -73,7 +77,12 @@ def commit(b_A):
 
 	return c, suffix
 
-
+""" Check if the hash value of the claimed result of the coinflip and the 
+given suffix corresponds to the hash value received before.
+@b_B: value of the coin (0 or 1)
+@c_B: hash value from coin and suffix
+@suffix_B: suffix 
+@return: Boolean value"""
 def reveal(b_B, c_B, suffix_B):
 
 	h = SHA256.new()
@@ -86,5 +95,7 @@ def reveal(b_B, c_B, suffix_B):
 	else:
 		return False
 
-AliceCoinFlip()	
+""" Start Alice """
+if __name__ == "__main__":
+	AliceCoinFlip()	
 	
--- 12-01/CoinFlip/Bob.py
+++ 12-01/CoinFlip/Bob.py
@@ -2,6 +2,15 @@ import socket, pickle
 from Crypto.Random import random
 from Crypto.Hash import SHA256
 
+
+""" Start Bob before you start Alice!!! """
+
+""" Bob waits for connection from Alice and in order to receive the hash value
+of her coin. Then Bob throws his coin and sends the hash value of the coin and 
+random suffix to Alice. Alice reveals the value of her coin and Bob reveals 
+his. Bob checks, if Alice told him the truth. The final result is the XOR 
+value of both coints.
+This implementation uses the reflection server!!! """
 def BobCoinFlip():
 	
 	host_A = "localhost"
@@ -14,31 +23,25 @@ def BobCoinFlip():
 	buffer_size = 1024 #bytes
 
 	# initialize socket
-	
 	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
 	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) #dont block when program crashes
 	s.bind((host_B, port_B))
 
 	# receive cipher from Alice
-
 	recdata, addr = s.recvfrom(1024)
-
 	coin_a = pickle.loads(recdata)
 	c_A = coin_a[2]
 
 	# generate coinflip and cipher
-
 	b_B = random.getrandbits(1)
 	c, suffix = commit(b_B)
 	print "Coin of Bob: " + str(b_B)
 
 	# send data to Alice
-
 	data = pickle.dumps((port_B, (host_A, port_A), c))
 	s.sendto(data, (rServer_ip, rServer_port))
 
 	# receive coinflip and nonce from Alice
-
 	recdata, addr = s.recvfrom(1024)
 	coin_a = pickle.loads(recdata)
 	b_A, suffix_A = coin_a[2]
@@ -46,12 +49,10 @@ def BobCoinFlip():
 	
 
 	# send coinflip and nonce to Alice
-
 	data = pickle.dumps((port_B, (host_A, port_A), (b_B, suffix)))
 	s.sendto(data, (rServer_ip, rServer_port))
 
 	# reveal
-
 	if (reveal(b_A, c_A, suffix_A)):
 		print "Alice tells the truth"
 		result = (b_B^b_A)
@@ -60,7 +61,10 @@ def BobCoinFlip():
 		print "Alice does not tell the truth"
 
 
-	
+""" add a random suffix to the value of the coin and generate its hash
+value
+@b_B: a random bit - can be 0 or 1 
+@return: hash value and random suffix"""	
 def commit(b_B):
 	
 	Min = 255 # bits
@@ -78,7 +82,12 @@ def commit(b_B):
 
 	return c, suffix
 
-
+""" Check if the hash value of the claimed result of the coinflip and the 
+given suffix corresponds to the hash value received before.
+@b_A: value of the coin (0 or 1)
+@c_A: hash value from coin and suffix
+@suffix_A: suffix 
+@return: Boolean value"""
 def reveal(b_A, c_A, suffix_A):
 
 	h = SHA256.new()
@@ -90,6 +99,8 @@ def reveal(b_A, c_A, suffix_A):
 		return True
 	else:
 		return False
-	
-BobCoinFlip()
+
+""" Start Bob """
+if __name__ == "__main__":
+	BobCoinFlip()
 	
